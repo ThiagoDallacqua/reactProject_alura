b6e3aac28cbed692a87e7a26679aeca964c582a6
Thiago Dallacqua <thiagodallacqua@outlook.com>
2017-05-11 22:37:28 -0300

    installed and bootstraped the react-router into the project

--- cdc-admin/src/App.js
+++ cdc-admin/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import AutorBox from './Autor.js';
+import HomeBox from './Home';
+import AutorBox from './Autor';
+import {Link,   Route,  BrowserRouter as Router, Switch} from 'react-router-dom';
 import './css/pure-min.css';
 import './css/side-menu.css'
 
 class App extends Component {
   render() {
     return (
+    <Router>
       <div id="layout">
           <a href="#menu" id="menuLink" className="menu-link">
               <span></span>
@@ -16,22 +19,28 @@ class App extends Component {
                   <a className="pure-menu-heading" href="#">Company</a>
 
                   <ul className="pure-menu-list">
-                      <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                      <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
-                      <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+                      <li className="pure-menu-item"><Link to="/" className="pure-menu-link">Home</Link></li>
+                      <li className="pure-menu-item"><Link to="/autor" className="pure-menu-link">Autor</Link></li>
+                      <li className="pure-menu-item"><Link to="#" className="pure-menu-link">Livro</Link></li>
                   </ul>
               </div>
           </div>
 
           <div id="main">
             <div className="header">
-              <h1>Cadastro de Autores</h1>
+              <h1>Bem vindo ao sistema</h1>
             </div>
             <div className="content" id="content">
-              <AutorBox/>
+              <Switch>
+                <Route path="/" exact component={HomeBox}/>
+                <Route path="/autor" component={ AutorBox }/>
+                <Route path="/livro"/>
+                <Route component={HomeBox} /> {/* No match path */}
+              </Switch>
             </div>
           </div>
       </div>
+    </Router>
     );
   }
 }
